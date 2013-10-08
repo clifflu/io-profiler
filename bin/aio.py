53c1dbd4daa462f87e7ddc6f845c6a474811dfe7
Cliff Chao-kuan Lu <clifflu@gmail.com>
2013-10-08 10:11:51 +0000

    test

--- bin/aio.py
+++ bin/aio.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+import boto
+import urllib
 
-URI_META = "http://169.254.169.254/latest/meta-data"
+conn = boto.connect_ec2()
+my_id = urllib.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read()
 
-def disks():
-    from urllib import urlopen
-    global URI_META
+def get_instance():
+  global conn, my_id
+  return conn.get_all_instances(instance_ids=[my_id])[0].instances[0]
 
-    for name in urlopen('%s/%s' % (URI_META, 'block-device-mapping')):
-        print(name)
+def get_sig():
+  i = get_instance()
+  return {
+    'id': i.id, 
+    'type': i.instance_type,
+    'tags': i.tags,
+    'ebs_optimized': i.ebs_optimized,
+  }
+
+
